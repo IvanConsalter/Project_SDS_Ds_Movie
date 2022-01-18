830da7ba99276b37ec7b9b10c6892d5c173dea02
Ivan Consalter <ivanconsalter1@gmail.com>
2022-01-18 09:13:27 -0300

    added spinner loader

--- frontend/src/pages/Listing/listing.tsx
+++ frontend/src/pages/Listing/listing.tsx
@@ -1,12 +1,19 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard/movieCard";
 import Pagination from "components/Pagination/pagination";
+
 import { useEffect, useState } from "react";
+
 import { MoviePage } from "types/movie";
+
 import { BASE_URL } from "utils/requests";
+
+import { TailSpin } from  'react-loader-spinner';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function Listing() {
   const [pageNumber, setPageNumber] = useState(0);
+  const [loading, setLoading] = useState(false);
   const [page, setPage] = useState<MoviePage>({
     content: [],
     last: true,
@@ -25,26 +32,31 @@ function Listing() {
       .then((response) => {
         const data = response.data as MoviePage;
         setPage(data);
+        setLoading(true);
       });
   }, [pageNumber]);
 
   const handlePageChange = (newPageNumber: number) => {
     setPageNumber(newPageNumber);
-  }
+  };
 
   return (
     <>
       <Pagination page={page} onChange={handlePageChange} />
-
       <div className="container">
-        <div className="row mx-3">
-          {page.content.map((movie) => (
-            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 my-3">
-              <MovieCard movie={movie} />
-            </div>
-          )  
-          )}
-        </div>
+        {loading ? (
+          <div className="row mx-3">
+            {page.content.map((movie) => (
+              <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 my-3">
+                <MovieCard movie={movie} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="d-flex justify-content-center mt-5">
+            <TailSpin color="#E50914" height={80} width={80} />
+          </div>
+        )}
       </div>
     </>
   );
