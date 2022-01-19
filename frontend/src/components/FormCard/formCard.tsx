fd50634caa693e74dd6a6b8cbd7e8e5718556df6
Ivan Consalter <ivanconsalter1@gmail.com>
2022-01-19 08:20:01 -0300

    added spinner loader in formCard

--- frontend/src/components/FormCard/formCard.tsx
+++ frontend/src/components/FormCard/formCard.tsx
@@ -4,6 +4,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
 import { BASE_URL } from "utils/requests";
 import { validateEmail } from "utils/validate";
+
+import { TailSpin } from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "./formCard.css";
 
 type Props = {
@@ -11,81 +14,90 @@ type Props = {
 };
 
 function FormCard({ movieId }: Props) {
-
   const navigate = useNavigate();
 
-	const [movie, setMovie] = useState<Movie>();
+  const [movie, setMovie] = useState<Movie>();
+  const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		axios.get(`${BASE_URL}/movies/${movieId}`)
-			.then(response => {
-				setMovie(response.data);
-			})
-	}, [movieId]);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies/${movieId}`).then((response) => {
+      setMovie(response.data);
+      setLoading(true);
+    });
+  }, [movieId]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-
     event.preventDefault();
 
     const email = (event.target as any).email.value;
     const score = (event.target as any).score.value;
 
-    if(!validateEmail(email)) {
+    if (!validateEmail(email)) {
       return;
     }
 
     const config: AxiosRequestConfig = {
       baseURL: BASE_URL,
-      method: 'PUT',
-      url: '/scores',
+      method: "PUT",
+      url: "/scores",
       data: {
         email: email,
         movieId: movieId,
-        score: score
-      }
-    }
+        score: score,
+      },
+    };
 
-    axios(config).then(response => {
-      navigate('/');
-    })
-  }
+    axios(config).then((response) => {
+      navigate("/");
+    });
+  };
 
   return (
-    <div className="dsmovie-form-container">
-      <img
-        className="dsmovie-movie-card-image"
-        src={movie?.image}
-        alt={movie?.title}
-      />
-      <div className="dsmovie-card-bottom-container">
-        <h3>{movie?.title}</h3>
-        <form className="dsmovie-form" onSubmit={handleSubmit}>
-          <div className="form-group dsmovie-form-group">
-            <label htmlFor="email">Informe seu email</label>
-            <input type="email" className="form-control" id="email" />
-          </div>
-          <div className="form-group dsmovie-form-group">
-            <label htmlFor="score">Informe sua avaliação</label>
-            <select className="form-control" id="score">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-          <div className="dsmovie-form-btn-container">
-            <button type="submit" className="btn btn-primary dsmovie-btn">
-              Salvar
-            </button>
-          </div>
-        </form>
+    <>
+      {loading ? (
+        <div className="dsmovie-form-container">
+          <img
+            className="dsmovie-movie-card-image"
+            src={movie?.image}
+            alt={movie?.title}
+          />
+          <div className="dsmovie-card-bottom-container">
+            <h3>{movie?.title}</h3>
+            <form className="dsmovie-form" onSubmit={handleSubmit}>
+              <div className="form-group dsmovie-form-group">
+                <label htmlFor="email">Informe seu email</label>
+                <input type="email" className="form-control" id="email" />
+              </div>
+              <div className="form-group dsmovie-form-group">
+                <label htmlFor="score">Informe sua avaliação</label>
+                <select className="form-control" id="score">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </div>
+              <div className="dsmovie-form-btn-container">
+                <button type="submit" className="btn btn-primary dsmovie-btn">
+                  Salvar
+                </button>
+              </div>
+            </form>
 
-        <Link to="/">
-          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-        </Link>
-      </div>
-    </div>
+            <Link to="/">
+              <button className="btn btn-primary dsmovie-btn mt-3">
+                Cancelar
+              </button>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div className="d-flex justify-content-center mt-5">
+          <TailSpin color="#E50914" height={80} width={80} />
+        </div>
+      )}
+    </>
   );
 }
 
